433be985faf255b64650242f710291080f6d654c
Adam Smith <adamps724@gmail.com>
2024-07-25 13:23:52 -0500

    reconfigured client folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,8 @@
-import { Routes, Route } from "react-router-dom"
-import { Post } from "./components/Post"
-import { PostList } from "./components/PostLists"
-import { PostProvider } from "./contexts/PostContext"
 
 function App() {
   return (
-    <div className="container">
-      <Routes>
-        <Route path="/" element={<PostList />} />
-        <Route
-          path="/posts/:id"
-          element={
-            <PostProvider>
-              <Post />
-            </PostProvider>
-          }
-        />
-      </Routes>
-    </div>
-  )
+    <h1>Hello World</h1>
+  );
 }
 
-export default App
+export default App;
