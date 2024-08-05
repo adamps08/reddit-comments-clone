f1f011ebcbfd600182ac9afd8fb5bad582aa0e36
Adam Smith <adamps724@gmail.com>
2024-08-04 21:05:44 -0500

    added useAsync.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
+import {Routes, Route} from "react-router-dom"
 import { PostList } from "./components/PostList";
 
 function App() {
+  return
+  <div className="container">
+    <Routes>
+      <Route path = "/" element= {<PostList />} />
+      <Route path = "/posts/:id" element= {<h1>Post</h1>} />
+    </Routes>
+  </div>
   return (
     <PostList />
   );
--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -1,10 +1,15 @@
 import React, {useState, useEffect} from "react"
+import { Link } from "react-router-dom"
+import { useAsync } from "../hooks/useAsync"
 import { getPosts } from "../services/posts"
 
 export function PostList() {
-    const [posts, setPosts] = useState([])
-    useEffect(()=> {
-        getPosts().then(setPosts)
-    }, [])
-    return <h1>{JSON.stringify(posts)}</h1>
+    const { loading, error, value: posts } = useAsync(getPosts)
+    return  posts.map(post => {
+        return (
+            <h1 key={post.id}>
+                <Link to= {`/posts/${post.id}`}>{post.title}</Link>
+            </h1>
+        )
+    })
 }
