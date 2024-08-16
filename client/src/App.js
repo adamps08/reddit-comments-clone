ee6177cf166e8c3ebbdb9ad3e930cabf23ff2763
Adam Smith <adamps724@gmail.com>
2024-08-15 20:38:50 -0500

    post content now appears

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import { Routes, Route } from "react-router-dom";
+import { Post } from "./components/Post";
 import { PostList } from "./components/PostList";
+import { PostProvider } from "./contexts/PostContext";
+
 
 function App() {
   return (
     <div className="container">
       <Routes>
         <Route path="/" element={<PostList />} />
-        <Route path="/posts/:id" element={<h1>Post</h1>} />
+        <Route 
+          path="/posts/:id" 
+          element={
+            <PostProvider>
+              <Post />
+            </PostProvider>
+          } 
+        />
       </Routes>
     </div>
   );
